bb5a1182862d8f9e35ac43434d6051341efa56e1
admin <stychinskiisv@gmail.com>
2026-03-11 23:49:02 +0300

    Chore: add jwt secret

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,12 +7,14 @@ import (
 type Config struct {
 	Port        string
 	DatabaseURL string
+	JWTSecret   string
 }
 
 func Load() (*Config, error) {
 	cfg := &Config{
 		Port:        getEnv("PORT", "8080"),
 		DatabaseURL: getEnv("DATABASE_URL", ""),
+		JWTSecret:   getEnv("JWT_SECRET", ""),
 	}
 
 	return cfg, nil
