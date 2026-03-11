f46426c8ad3a63c216311c727b1009b36ca53f0d
admin <stychinskiisv@gmail.com>
2026-03-11 15:16:12 +0300

    Chore: add logger

---
+++ internal/middleware/logger.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+
+		c.Next()
+
+		latency := time.Since(start)
+		status := c.Writer.Status()
+
+		logrus.Infof("%s %s %d %s", c.Request.Method, c.Request.URL.Path, status, latency)
+	}
+}
