2092d177530be3021c9c3ae179a1dc6111b92c67
admin <stychinskiisv@gmail.com>
2026-03-11 15:16:21 +0300

    Chore: add recovery

---
+++ internal/middleware/recovery.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func Recovery() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				logrus.Errorf("panic recovered: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+				c.Abort()
+			}
+		}()
+		c.Next()
+	}
+}
