9fe790349bb3d667f2cbbac69472bcb557ee0550
admin <stychinskiisv@gmail.com>
2026-03-12 14:52:45 +0300

    WIP: add sign in/up requests

---
+++ internal/modules/auth/dto/register.go
@@ -0,0 +1,7 @@
+package dto
+
+type RegisterRequest struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
