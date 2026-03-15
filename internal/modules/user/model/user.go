336d4618c51ba956459b0ff9a8e3513d36a93e84
admin <stychinskiisv@gmail.com>
2026-03-15 15:52:02 +0300

    Chore: fix user model

--- internal/modules/user/model/user.go
+++ internal/modules/user/model/user.go
@@ -4,14 +4,13 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
 	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
-	Email     string    `gorm:"uniqueIndex;not null" json:"email"`
-	Name      string    `gorm:"not null" json:"name"`
+	Username  string    `gorm:"unique;not null" json:"username"`
+	Phone     string    `gorm:"not null" json:"phone"`
+	Email     string    `gorm:"unique;not null" json:"email"`
 	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
